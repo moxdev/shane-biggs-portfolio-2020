b34e72889d33d4a3ca1348512a86916c00bcf250
Shane Biggs <moxdev1@gmail.com>
2020-04-17 13:03:18 -0400

    basic setup w/o mdx

---
+++ config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  siteTitle: `Shane Biggs Web Developer`, // Site title.
+  siteTitleAlt: `Shane Biggs Web Developer`, // Alternative site title for SEO.
+  siteLogo: `/icons/icon-512x512.png`, // Logo used for SEO and manifest.
+  siteUrl: `https://shanebiggs.com/`, // Domain of your website without pathPrefix.
+  pathPrefix: '', // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-business/.
+  siteDescription: `A web developer portfolio web site for Shane Biggs.`, // Website description used for RSS feeds/meta description tag.
+  siteRss: `/rss.xml`,
+  siteFBAppID: '', // FB Application ID for using app insights
+  googleTagManagerID: '', // GTM tracking ID.
+  disqusShortname: `shane-biggs-portfolio`, // Disqus shortname.
+  author: `Shane Biggs`,
+  userTwitter: `moxbiggs`,
+  userLocation: `US`,
+  userDescription: '',
+  copyright: `Copyright © Shane Biggs Web Development 2020. All Rights Reserved.`, // Copyright string for the footer of the website and RSS feed.
+  themeColor: `#00d1b2`, // Used for setting manifest and progress theme colors.
+  backgroundColor: `#ffffff`, // Used for setting manifest background color.
+}
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,22 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
+const config = require('./config')
+
+const pathPrefix = config.pathPrefix === '/' ? '' : config.pathPrefix
 
 module.exports = {
-  /* Your site config here */
   siteMetadata: {
-    title: `Shane Biggs Web Developer`,
-    author: `Shane Biggs`,
-    description: `A web development portfolio website for Shane Biggs.`,
-    siteUrl: `https://shanebiggs.com/`,
+    title: config.siteTitle,
+    siteUrl: config.siteUrl,
+    author: config.author,
+    description: config.siteDescription,
+    rssMetadata: {
+      site_url: config.siteUrl + pathPrefix,
+      feed_url: config.siteUrl + pathPrefix + config.siteRss,
+      title: config.siteTitle,
+      description: config.siteDescription,
+      image_url: `${config.siteUrl + pathPrefix}/icons/icon-512x512.png`,
+      author: config.author,
+      copyright: config.copyright,
+    },
     social: [
       {
         name: `twitter`,
@@ -39,6 +45,13 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
         name: `images`,
         path: `${__dirname}/src/images`,
       },
@@ -51,6 +64,33 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: 'YOUR_GOOGLE_ANALYTICS_TRACKING_ID',
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ['/preview/**', '/do-not-track/me/too/'],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Enables Google Optimize using your container Id
+        optimizeId: 'YOUR_GOOGLE_OPTIMIZE_TRACKING_ID',
+        // Enables Google Optimize Experiment ID
+        experimentId: 'YOUR_GOOGLE_EXPERIMENT_ID',
+        // Set Variation ID. 0 for original 1,2,3....
+        variationId: 'YOUR_GOOGLE_OPTIMIZE_VARIATION_ID',
+        // Any additional optional fields
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: 'example.com',
+      },
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
